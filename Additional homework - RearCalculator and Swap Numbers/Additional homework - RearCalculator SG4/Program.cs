9b6f16b7face6995859746a8a45a0d1f3660d9ae
Gjorgji Kongulovski <46351282+Hellinho85@users.noreply.github.com>
2019-02-27 21:48:31 +0100

    Additional Homework - Gjorgji Kongulovski SG1

---
+++ Additional homework - RearCalculator and Swap Numbers/Additional homework - RearCalculator SG4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_homework___RearCalculator_SG4
+{
+    class RearCalculator
+    {
+        static void Main(string[] args)
+        {
+
+            /* Task no.1 - Create new console application “RealCalculator” that 
+            takes two numbers from the input and asks what operation would the user 
+            want to be done ( +, - , * , / ). Then it returns the result. If the user
+            enters a wrong operation display "Invalid operation" and terminate the program.
+            
+            Test Data:
+            Enter the First number: 10
+            Enter the Second number: 15
+            Enter the Operation: +
+ 
+            Expected Output:
+            The result is: 25 */
+
+            Console.WriteLine("Please enter your desired arithmetic operation:");
+            string ope = Console.ReadLine();
+
+            if (ope != "+" && ope != "-" && ope != "*" && ope != "/")
+            {
+                Console.WriteLine("Invalid operation selected!!!");
+                System.Environment.Exit(1);
+
+
+            }
+
+            Console.WriteLine("Enter the First number:");
+            string numberOne = Console.ReadLine();
+            int num1 = int.Parse(numberOne);
+
+            Console.WriteLine("Enter the second number:");
+            string numberTwo = Console.ReadLine();
+            int num2 = int.Parse(numberTwo);
+
+            if (ope == "+")
+            {
+                Console.WriteLine($"The result is: {num1 + num2}");
+            }
+            else if (ope == "-")
+            {
+                Console.WriteLine($"The result is: {num1 - num2}");
+            }
+            else if (ope == "*")
+            {
+                Console.WriteLine($"The result is: {num1 * num2}");
+            }
+            else if (ope == "/")
+            {
+                Console.WriteLine($"The result is: {num1 / num2}");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Additional homework - RearCalculator and Swap Numbers/SwapNumbers SG4 homework/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbers_SG4_homework
+{
+    class SwapNumbers
+    {
+        static void Main(string[] args)
+        {
+
+            /* Task no.3 - Create new console application “SwapNumbers”. 
+            Input 2 numbers from the console and then swap the values of 
+            the variables so that the first variable has the second value 
+            and the second variable the first value.
+            Please find example below:
+
+            Test Data:
+            Input the First Number: 5
+            Input the Second Number: 8
+ 
+            Expected Output:
+            After Swapping:
+            First Number: 8 
+            Second Number: 5 */
+
+
+            Console.WriteLine("Input the first number:");
+            string numb1 = Console.ReadLine();
+            int numby1 = int.Parse(numb1);
+            Console.WriteLine($"Before the swap the first number is equal to {numby1}");
+
+            Console.WriteLine("Input the second number:");
+            string numb2 = Console.ReadLine();
+            int numby2 = int.Parse(numb2);
+            Console.WriteLine($"Before the swap the first number is equal to {numby2}");
+
+            numby1 = numby1 + numby2;
+            numby2 = numby1 - numby2;
+            numby1 = numby1 - numby2;
+
+            Console.WriteLine($"After the swap the first number is equal to {numby1}");
+            Console.WriteLine($"After the swap the second number is equal to {numby2}");
+
+
+            Console.Read();
+
+        }
+    }
+}
